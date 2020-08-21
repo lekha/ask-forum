d43b8caf1149e9626e1c0ebd2b30a44feac455b4
lekha <305097+lekha@users.noreply.github.com>
2020-08-21 17:43:22 -0400

    Add mock text to aid CSS setup

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    This is the home page.
+  <div class="home">
+    <div class="header">
+      <h1>Project Name</h1>
+      <div class="search">Search bar</div>
+      <button>Log in</button>
+      <button>Register</button>
+    </div>
+    <div class="body">
+      <div class="sidebar">
+        <ul>
+          <li>Questions</li>
+          <li>Tags</li>
+          <li>Users</li>
+        </ul>
+      </div>
+      <div class="content">
+        <div class="title">
+          <div class="container">
+            <h2>Questions</h2>
+            <button>Ask a question</button>
+          </div>
+          <div>{{ numQuestions.toLocaleString() }} questions</div>
+        </div>
+        <div class="article">
+          <div v-for="question in questions" :key="question.id" class="question">
+            <h3>{{ question.title }}</h3>
+            <p>{{ question.description }}</p>
+            <div v-for="tag in question.tags" :key="tag.id" class="tag">
+              #{{ tag.text }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  props: {
+  data() {
+    return {
+      numQuestions: 1000,
+      questions: [
+        {
+          id: 0,
+          title: "Question 1 title",
+          description: "Question 1 description",
+          tags: [
+            {
+              id: 0,
+              text: "tag1",
+            },
+            {
+              id: 1,
+              text: "tag2",
+            },
+          ],
+        },
+        {
+          id: 1,
+          title: "Question 2 title",
+          description: "Question 2 description",
+          tags: [
+            {
+              id: 1,
+              text: "tag2",
+            },
+          ],
+        },
+      ],
+    }
   }
 }
 </script>
