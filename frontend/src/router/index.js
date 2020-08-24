028f20e1d64613b0fb0ca178d73d52f4510d2566
lekha <305097+lekha@users.noreply.github.com>
2020-08-23 21:30:35 -0400

    Add /questions route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "@/views/Home.vue"
+import Question from "@/views/Question.vue"
 
 
 Vue.use(VueRouter)
@@ -11,6 +12,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/questions/:id",
+    name: "Question",
+    component: Question,
   },
 ]
 
---
+++ frontend/src/views/Question.vue
@@ -0,0 +1,50 @@
+<template>
+  <base-layout>
+
+    <template v-slot:title>
+      <div class="container">
+        <h2>{{ title }}</h2>
+        <button>Ask a question</button>
+      </div>
+      <div>Asked {{ createdDate }}</div>
+    </template>
+
+    <template v-slot:article>
+      <p>{{ description }}</p>
+      <div>
+        <div v-for="tag in tags" :key="tag.id" class="tag">
+          #{{ tag.text }}
+        </div>
+      </div>
+    </template>
+
+  </base-layout>
+</template>
+
+<script>
+import Base from "@/layouts/Base.vue";
+
+export default {
+  name: "Question",
+  components: {
+    "base-layout": Base,
+  },
+  data() {
+    return {
+      createdDate: "2020-08-11",
+      description: "Question 1 description",
+      tags: [
+        {
+          id: 0,
+          text: "tag1",
+        },
+        {
+          id: 1,
+          text: "tag2",
+        },
+      ],
+      title: "Question 1 title",
+    }
+  }
+}
+</script>
